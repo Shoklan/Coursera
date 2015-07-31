d8955f15530650d9a1ec306a67f7d87b60c4a9e8
Collin Mitchell <Shoklansenta@gmail.com>
2015-07-31 06:03:10 -0400

    Documentation
    Added comments to clarify code

--- RProgramming/wk4/rankhospital.R
+++ RProgramming/wk4/rankhospital.R
@@ -1,8 +1,7 @@
 ##  AUTHOR: COLLIN MITCHELL
-##    DATE: 2015/7/29
+##    DATE: 2015/7/31
 ## PURPOSE: ASSIGNMENT WORK.
 
-# Going to need this work.
 # For work:
 #setwd("C:\\Users\\mitcolli\\Documents\\Docs\\code\\Coursera\\RProgramming\\wk4")
 
@@ -13,8 +12,8 @@ checkState <- function(state){
   return(FALSE)
 }
 
-best <- function(state, outcome){
-  # Read the Data.
+rankhospital <- function(state, outcome, ranking){
+  # Read the data:
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   # This is for my own reference to make debugging easier and for comparison.
@@ -24,6 +23,7 @@ best <- function(state, outcome){
   name     <- "Hospital.Name"
   
   # map inputs to comparable values.
+  # This makes debugging sooooo much easier!
   if(outcome == "heart attack")  outcome <- hAttack
   if(outcome == "heart failure") outcome <- hFailure
   if(outcome == "pneumonia")     outcome <- pSick
@@ -36,31 +36,6 @@ best <- function(state, outcome){
     stop("outcome is not a valid searchable condition.")
   }
   else{
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate.
-    
-    # Retrieve all possible hostpitals in the state:
-    temp <- subset(data, data$State == state)
-    # Debug: randomTemp <<- temp
-    
-    # pull out all number of outcomes and sort:
-    filterTarget<- sort(as.numeric(temp[,outcome]))
-    
-    # Debug: randomFilter <<- filterTarget
-    
-    # PUll out any duplicates in the data frame with the same number of
-    # occurances:
-    potentialResults <- subset(temp, temp[,outcome] == min(filterTarget))
-    
-    # Debug: random <<- potentialResults
-    
-    # If more than one result, sort and get first name:
-    if(length(potentialResults[outcome]) > 1){
-      result <- as.character(sort(potentialResults[name])[1])
-    } else {
-      # Just return the one item:
-      result <- as.character(potentialResults[name])
-    }
-    result
+  
   }
 }
