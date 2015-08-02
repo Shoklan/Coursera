d2024e6ecb293c77f675ca3febeb11bae4067abf
Collin Mitchell <Shoklansenta@gmail.com>
2015-08-02 03:33:53 -0400

    rankall.r seems to work
    ... Submit when I get home.

--- RProgramming/wk4/rankall.r
+++ RProgramming/wk4/rankall.r
@@ -5,11 +5,58 @@
 # For work:
 #setwd("C:\\Users\\mitcolli\\Documents\\Docs\\code\\Coursera\\RProgramming\\wk4")
 
-# Check to see if actually a State
-checkState <- function(state){
-  for(st in state.abb)
-    if(state == st) return(TRUE)
-  return(FALSE)
+filterState <- function(data, outcome, name, num){
+  # Create container for data
+  results <- data.frame(Hospital.Name = NA, State = NA)
+  count   <- 1
+  if(num == "worst") worstFlag <- TRUE
+  else               worstFlag <- FALSE
+  if(num == "best")        num <- 1
+  
+  # Begin going through data!
+  for(st in state.abb){
+    # Generate slice of state data
+    slice <- subset(data, data$State == st)
+    
+    
+    # The subset seems to return the data to char type
+    # so, convert and order after taking the slice
+    slice[,outcome] = as.numeric(slice[,outcome])
+    #convertToNum <- sapply(slice[,outcome], as.numeric)
+    #slice[,outcome] = convertToNum
+    
+    
+    reference <- slice[ order(slice[,outcome], slice[,name]), ]
+    if(worstFlag) num <- length(slice[,1])
+    #DEBUG:
+    print(count)
+    print(st)
+    randomSlice <<- slice
+    randomReference <<- reference
+    
+    if(count == 1){
+      results[1,name]  <- reference[num,name]
+      results[1,"State"] <- reference[num,"State"]
+    }
+    else if(num > length(reference[,1])){
+      results[count,name]    <- NA
+      results[count,"State"] <- st
+      print(c("NUmber of Hospitals", length(reference[,1])))
+      print(c("Hospital Name: ", reference[num,name]))
+      print(reference[num,st])
+    }else{
+      results[count,name]  <- reference[num,name]
+      results[count,"State"] <- reference[num,"State"]
+    }
+    
+    count <- count +1
+  }
+  
+  # Now I get to sort this too...
+  # reference <- slice[ order(slice[,outcome], slice[,name]), ]
+  randomResults <<- results
+  results[ order(randomResults[,"State"], randomResults[,name]), ]
+  results
 }
 
 rankall <- function(outcome, num="best"){
@@ -24,6 +71,21 @@ rankall <- function(outcome, num="best"){
   
   # map inputs to comparable values.
   # This makes debugging sooooo much easier!
-  if(outcome == "heart attack")  outcome <- hAttack
-  if(outcome == "heart failure") outcome <- hFailure
-  if(outcome == "pneumonia")     outcome <- pSick
+  if(     outcome == "heart attack")  outcome <- hAttack
+  else if(outcome == "heart failure") outcome <- hFailure
+  else if(outcome == "pneumonia")     outcome <- pSick
+  else { stop("invalid outcome")}
+  
+
+  #convertToNum <- sapply(data[,outcome], as.numeric)
+  #data[,outcome] = convertToNum
+  #print(outcome)
+  #randomData <<- data
+  ## For each state, find the hospital of the given rank
+  filterState(data, outcome, name, num)
+  
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+  #results <- data.frame(Hospital.Name, State = st)
+    
+  }
