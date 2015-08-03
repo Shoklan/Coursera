ae5ef0f5a304877a51c067699cfe2f0f006d1b87
Shoklan <Shoklansenta@gmail.com>
2015-08-03 11:47:21 -0400

    Finalized.
    R Programming Completed.

--- RProgramming/wk4/rankall.r
+++ RProgramming/wk4/rankall.r
@@ -2,13 +2,12 @@
 ##    DATE: 2015/8/1
 ## PURPOSE: ASSIGNMENT WORK.
 
-# For work:
-#setwd("C:\\Users\\mitcolli\\Documents\\Docs\\code\\Coursera\\RProgramming\\wk4")
-
+# subset by state, pullout out data, then return ordered results.
 filterState <- function(data, outcome, name, num){
   # Create container for data
   results <- data.frame(hospital = NA, state = NA)
   count   <- 1
+  # grab text results for later
   if(num == "worst") worstFlag <- TRUE
   else               worstFlag <- FALSE
   if(num == "best")        num <- 1
@@ -24,45 +23,40 @@ filterState <- function(data, outcome, name, num){
     # The subset seems to return the data to char type
     # so, convert and order after taking the slice
     slice[,outcome] = as.numeric(slice[,outcome])
-    #convertToNum <- sapply(slice[,outcome], as.numeric)
-    #slice[,outcome] = convertToNum
     
-    
+    # order data based on outcome[min->max], name[alphabetical]
     reference <- slice[ order(slice[,outcome], slice[,name]), ]
+    # is worst, always set index to last element
     if(worstFlag) num <- length(slice[,1])
-    #DEBUG:
-    #print(count)
-    #print(st)
-    randomSlice <<- slice
-    randomReference <<- reference
     
+    #if first run, the replace NA values
     if(count == 1){
       results[1,"hospital"]  <- reference[num,name]
       results[1,"state"] <- reference[num,"State"]
     }
+    #if element doesn't exist, then return NA
     else if(num > length(reference[,1])){
       results[count,"hospital"]    <- NA
       results[count,"state"] <- st
-      #print(c("NUmber of Hospitals", length(reference[,1])))
-      #print(c("Hospital Name: ", reference[num,name]))
-      #print(reference[num,st])
-    }else{
+    }
+    # else insert element into results.
+    else{
       results[count,"hospital"]  <- reference[num,name]
       results[count,"state"] <- reference[num,"State"]
     }
     
     count <- count +1
   }
-  
-  # Now I get to sort this too...
-  # reference <- slice[ order(slice[,outcome], slice[,name]), ]
-  
+
+  # order the results since the states are not being inserted
+  # alphabetically
   results[ order(randomResults[,"state"], randomResults[,"hospital"]), ]
-  randomResults <<- results
-  #print(results)
   results
 }
 
+
+# This is the main function;
+# Use this when running!
 rankall <- function(outcome, num="best"){
   # Read the data:
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -81,15 +75,6 @@ rankall <- function(outcome, num="best"){
   else { stop("invalid outcome")}
   
 
-  #convertToNum <- sapply(data[,outcome], as.numeric)
-  #data[,outcome] = convertToNum
-  #print(outcome)
-  #randomData <<- data
   ## For each state, find the hospital of the given rank
   filterState(data, outcome, name, num)
-  
-  ## Return a data frame with the hospital names and the
-  ## (abbreviated) state name
-  #results <- data.frame(Hospital.Name, State = st)
-    
-  }
+}
