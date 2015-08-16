8c9098d5747ca95e39307a1e95fe6f6ed3634caf
Collin Mitchell <Shoklansenta@gmail.com>
2015-08-16 06:56:36 -0400

    Framework for Project 2
    Base plot files for the final project.

---
+++ DataAnalysis/plot2.R
@@ -0,0 +1,8 @@
+# AUTHOR: COLLIN MITCHELL
+# DATE: 2015/8/16
+# PURPOSE:
+#        | This program is Project 2 of the Expolatory Data Analysis project.
+#        | The purpose is to explore the creation of graphs created by the plotting
+#        | system.
+#        | Graph #2
+####################################################################################
