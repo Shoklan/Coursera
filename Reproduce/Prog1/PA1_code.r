4d46e01c4a58e98907abac847ce6623fd56aa46d
Collin Mitchell <Shoklansenta@gmail.com>
2015-09-17 05:59:52 -0400

    Almost Done with Project 1
    Update.

--- Reproduce/Prog1/PA1_code.r
+++ Reproduce/Prog1/PA1_code.r
@@ -8,34 +8,72 @@ baseurl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
 file.dir <- "./data"
 completepath <- paste(file.dir, "/data.zip", sep = "")
 
+cumulativeSums     <- 0
+cumulativeMeans    <- 0
+cumulativeMedians  <- 0
+cumulativeInterval <- 0
 
-# Get a
+## Libraries
+library(dplyr)
+library(reshape2)
+
+# Prepare environment for data and get data.
 if(!file.exists(file.dir)){ dir.create("./data")}
+
 download.file(baseurl, destfile = completepath)
 filename <- unzip(completepath, exdir=file.dir)
-
 data <- read.csv(filename)
 
-#Step 1
-# Load the data (i.e. read.csv())
-# Process/transform the data (if necessary) into a format suitable for your analysis
+
+# Arrange Data based on the dates.
+data <- arrange(data, date)
+
 
 # Challenge 1
 # mean steps per day?
 ## Ignore NA values (.., rm.na = T)
+index = 1
+
+for(distinctdates in levels(data$date)){
+  temp <- filter(data, date == distinctdates)
+  cumulativeSums[index] = sum(temp$steps, rm.na = T)
+  index <- index + 1
+}
+
 
 # Challenge 2
 # Histogram of values
 # Report mean & median steps/day
+for(distinctdates in levels(data$date)){
+  temp <- filter(data, date == distinctdates)
+  cumulativeMeans[index] = sum(temp$steps, rm.na = T)
+  index <- index + 1
+}
+
+for(distinctdates in levels(data$date)){
+  temp <- filter(data, date == distinctdates)
+  cumulativeMedians[index] = sum(temp$steps, rm.na = T)
+  index <- index + 1
+}
+
+# Histogram?
+
 
 # Challenge 3
 # Time series plot: 5-minute interval, avg steps taken, across all days
 # Which 5 minute interval has the most steps.
 
+splitOnInterval <- dcast(data, date + steps ~ interval)
+
+for(index in 3:ncol(splitOnInterval) ){
+  cumulativeInterval[index-2] <- sum(splitOnInterval[,index], na.rm = T)
+}
+  
 
 # Challenge 4
 # n of NAs in data?
 # deal with NA's in data and substitute solution in copy of data
+
 
 # Challenge 5 | NEW DATASET
 # Histogram of number of steps/day
