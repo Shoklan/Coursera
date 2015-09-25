3dbd6040e0127a9e9d6327d50de4ddef37ef8d2c
Collin Mitchell <Shoklansenta@gmail.com>
2015-09-25 05:52:15 -0400

    Reproduce Project
    Basic framework.

--- Reproduce/prog2/ReproduceProject2.R
+++ Reproduce/prog2/ReproduceProject2.R
@@ -8,22 +8,58 @@
 #
 ## Challenges:
 # 1] Across the United States, which types of events (as indicated in the EVTYPE variable) are most harmful with respect to population health?
+#   P # EVTYPE
+#     # FATALITIES 
+#     # INJURIES
+
 # 2] Across the United States, which types of events have the greatest economic consequences?
+#   P # EVTYPE
+#     # PROPDMG
+#     # PROPDMGEXP
+#     # CROPDMG
+#     # CROPDMGEXP
+
+# PROPDMG
+# EVTYPE 
 ################################################
 
 ###
 # Variables
-
 dataUrl   <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
 file.dir  <- "./data"
-
 completepath <- paste(file.dir, "/data.csv.bz2", sep = "")
+cumulativeSum <- 
+
+# Libraries
+library(dplyr)
 
 
-# Prepare environment for data and get data.
+# Prepare environment for data and get data:
 if(!file.exists(file.dir)){ dir.create("./data")}
 
 # Extract Data:
 download.file(dataUrl, destfile = completepath)
+
 filename <- bzfile(completepath)
 data <- read.csv(filename)
+
+
+# Title
+
+# Synopsis
+
+# Data Processing
+# 1] Select type
+# 2] Sum consequences
+# 3] Report results.
+
+index <- 1
+
+for(item in levels(data$EVTYPE)){
+  tempslice <- filter(data, EVTYPE == item)
+  cumulativeSum <- c(cumulativeSum, sum(tempslice$FATALITIES) + sum(tempslice$INJURIES))
+}
+
+storage <- data.frame(type = levels(data$EVTYPE), sums=cumulativeSum)
+
+# Results
